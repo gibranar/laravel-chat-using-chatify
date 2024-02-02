48466078b3c5852a7ac48ee63622a4a490467d55
Gib-byte <gibranabyan082@gmail.com>
2024-02-02 20:52:23 +0700

    Init commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,24 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+        <div class="max-w-7xl py-6 mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 w-full flex justify-end text-gray-900 dark:text-gray-100">
+                    {{-- <a href="{{ route('chat.show') }}" type="button" class="btn btn-blue">Go to Chat</a> --}}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
